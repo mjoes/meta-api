f797762275f0a67b4a44fda97423f3a945bf39af
mjoes <morten.slingsby@gmail.com>
2025-01-20 20:03:48 +0100

    save

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Metadata api";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/24.11-beta";
+  };
+
+  outputs = { self, nixpkgs }@inputs:
+  let 
+    system = "aarch64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        python3Packages.requests
+      ];
+      shellHook = ''
+        source .env
+      '';
+    };
+  };
+}
