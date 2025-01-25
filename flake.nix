958ab4cf2cf7ba1a9306eae6d3cc4ec66a61a181
mjoes <morten.slingsby@gmail.com>
2025-01-25 22:04:45 +0100

    eachSystem

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/24.11-beta";
     treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
@@ -11,45 +12,53 @@
       self,
       nixpkgs,
       treefmt-nix,
+      flake-utils,
     }@inputs:
-    let
-      system = "aarch64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
+    flake-utils.lib.eachSystem
+      [
+        "aarch64-linux"
+        "x86_64-linux"
+      ]
+      (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
 
-      insert_app = import ./packages/insert_app/default.nix { python = pkgs.python3; };
-      treefmtEval = treefmt-nix.lib.evalModule pkgs {
-        projectRootFile = "flake.nix";
-        programs = {
-          nixfmt.enable = true;
-          ruff-format.enable = true;
-          ruff-check.enable = true;
-        };
-      };
+          insert_app = import ./packages/insert_app/default.nix { python = pkgs.python3; };
+          treefmtEval = treefmt-nix.lib.evalModule pkgs {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              ruff-format.enable = true;
+              ruff-check.enable = true;
+            };
+          };
 
-    in
-    {
+        in
+        {
 
-      formatter.${system} = treefmtEval.config.build.wrapper;
+          formatter = treefmtEval.config.build.wrapper;
 
-      packages.${system} = {
-        # insert_app = import ./packages/insert_app/default.nix { python = pkgs.python3; };
-        docker-insert-app = import ./docker/insert_app.nix {
-          inherit pkgs;
-          insert-app = insert_app;
-        };
-      };
+          packages = {
+            # insert_app = import ./packages/insert_app/default.nix { python = pkgs.python3; };
+            docker-insert-app = import ./docker/insert_app.nix {
+              inherit pkgs;
+              insert-app = insert_app;
+            };
+          };
 
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          docker
-          python3Packages.flask
-        ];
-        shellHook = ''
-          source .env
-          export PYTHONPATH=$PYTHONPATH:$PWD/packages/insert_app/src
-        '';
-      };
-    };
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              docker
+              python3Packages.flask
+            ];
+            shellHook = ''
+              source .env
+              export PYTHONPATH=$PYTHONPATH:$PWD/packages/insert_app/src
+            '';
+          };
+        }
+      );
 }
